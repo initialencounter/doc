a2043c55a6114736a1f4a9f35cc4582bd9021c2c
风宝宝 <109729945+initialencounter@users.noreply.github.com>
2024-05-06 16:41:32 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,10 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCode = require('prism-react-renderer');
-const lightCodeTheme = lightCode.themes.github;
-const darkCode = require('prism-react-renderer');
-const darkCodeTheme = darkCode.themes.dracula;
+const lightCodeTheme = require('prism-react-renderer').themes.github;
+const darkCodeTheme = require('prism-react-renderer').themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
